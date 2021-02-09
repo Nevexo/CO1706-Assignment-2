7758bb1212e5ae0e7cb75d9e30d70e3ad1c1e3f8
Cameron Fleming <cameron@nevexo.space>
2021-02-09 00:17:40 +0000

    Add flags to dev.ps1

--- dev.ps1
+++ dev.ps1
@@ -1,10 +1,29 @@
-$DockerDesktopProcess = Get-Process 'Docker Desktop'
-if (-Not $DockerDesktopProcess) {
-  Write-Host "Docker is not running, attempting to start."
-  Start-Process "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-  Write-Host "Waiting 10 seconds for docker to start"
-  Start-Sleep -s 10
+param (
+    [switch]$Up,
+    [switch]$Down,
+    [switch]$KillDocker
+)
+
+if ($Up) {
+  $DockerDesktopProcess = Get-Process -Name 'Docker Desktop' -ErrorAction SilentlyContinue
+  if (-Not $DockerDesktopProcess) {
+    Write-Host "Docker is not running, attempting to start."
+    Start-Process "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+    Write-Host "Waiting 10 seconds for docker to start"
+    Start-Sleep -s 10
+  }
+
+  Write-Host "Starting containers"
+  docker-compose up -d
 }
 
-Write-Host "Starting containers"
-docker-compose up -d
+
+if ($Down) {
+  Write-Host "Bringing down containers..."
+  docker-compose down
+
+  if ($KillDocker) {
+    Write-Host "Shutting down Docker..."
+    Stop-Process -Name "Docker Desktop"
+  }
+}
