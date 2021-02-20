4f69fc6dfbb2e973c57fd094b0a913b2949314d9
Cameron Fleming <cameron@nevexo.space>
2021-02-20 17:57:12 +0000

    Inital bootstrap + PHP PDO connection

---
+++ src/php/database.php
@@ -0,0 +1,10 @@
+<!-- Database Interaction Functions -->
+<?php
+require 'vars.php';
+
+// PDO Data source name for MySQL server as configured in vars.php
+$DATABASE_DSN = "mysql:host=" . $DATABASE_HOST . ";dbname=" . $DATABASE_SCHEMA_NAME;
+
+// Establish connection to database
+$pdo = new PDO($DATABASE_DSN, $DATABASE_USERNAME, $DATABASE_PASSWORD);
+?>
---
+++ src/php/vars.php
@@ -0,0 +1,8 @@
+<!-- Variables used by other PHP files -->
+<?php
+// Database Configuration
+$DATABASE_HOST = 'sql';
+$DATABASE_USERNAME = 'root';
+$DATABASE_PASSWORD = 'password';
+$DATABASE_SCHEMA_NAME = 'musicstream';
+?>
